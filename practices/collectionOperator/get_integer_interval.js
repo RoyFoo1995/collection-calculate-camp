7beeb6f705873f17a7c48c6afc088016d44d8eee
yifu <yifu@thoughtworks.com>
2018-07-16 16:04:27 +0800

    pass the test get_integer_interval

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -1,7 +1,24 @@
 'use strict';
 
 function get_integer_interval(number_a, number_b) {
-  //在这里写入代码
+  let arry = generateArry(number_a,number_b);
+
+  return arry;
+}
+
+function generateArry(number_a, number_b) {
+  let arry = [];
+  if (number_a > number_b) {
+    for (var i = number_a; i >= number_b; i--) {
+      arry.push(i);
+    }
+  } else {
+    for (var i = number_a; i <= number_b; i++) {
+      arry.push(i);
+    }
+  }
+  console.log(arry);
+  return arry;
 }
 
 module.exports = get_integer_interval;
