40d3630ed74f2f52979fb411dfdecfa4c2494916
yifu <yifu@thoughtworks.com>
2018-07-16 16:52:59 +0800

    pass the test get_union

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -1,7 +1,10 @@
 'use strict';
 
 function get_union(collection_a, collection_b) {
-  //在这里写入代码
+  collection_b = collection_b.filter(value => {
+    return !collection_a.includes(value);
+  });
+  return collection_a.concat(collection_b);
 }
 
 module.exports = get_union;
