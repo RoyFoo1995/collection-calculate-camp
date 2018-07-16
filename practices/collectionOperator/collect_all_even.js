75397b61a8c91b4c58d2521cc6f7637099e5ca1b
yifu <yifu@thoughtworks.com>
2018-07-16 11:19:50 +0800

    change the test collect_all_even

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -2,8 +2,8 @@
 
 function collect_all_even(collection) {
 
-  var isEven = i => i % 2 != 0;
-  return collection.filter(((subArr, idx) => isEven(idx)));
+  var isEven = i => i % 2 == 0;
+  return collection.filter(((word) => isEven(word)));
 }
 
 module.exports = collect_all_even;
