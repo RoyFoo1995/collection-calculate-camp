a05ea738ed86249b1352bef54299fe9f8e3a88ed
yifu <yifu@thoughtworks.com>
2018-07-16 16:53:40 +0800

    pass the choose_no_repeat

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -1,8 +1,7 @@
 'use strict';
 
 function choose_no_repeat_number(collection) {
-
-  //在这里写入代码
+  return collection.filter((value, index, arry) => value != arry[index + 1]);
 }
 
 module.exports = choose_no_repeat_number;
