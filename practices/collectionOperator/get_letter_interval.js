9a73674cf01dce38fdbf2ff7d9a70832771c92d1
yifu <yifu@thoughtworks.com>
2018-07-16 16:52:27 +0800

    pass the test get_letter_interval

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -1,7 +1,40 @@
 'use strict';
 
+var charArry = "abcdefghijklmnopqrstuvwxyz";
+
 function get_letter_interval(number_a, number_b) {
-  //在这里写入代码
+  let arry = generateArry(number_a, number_b);
+  let stringArry = [];
+  arry.forEach(value => {
+    stringArry.push(covert(value - 1));
+  });
+  return stringArry;
+}
+
+function generateArry(number_a, number_b) {
+  let arry = [];
+  if (number_a > number_b) {
+    for (var i = number_a; i >= number_b; i--) {
+      arry.push(i);
+    }
+  } else {
+    for (var i = number_a; i <= number_b; i++) {
+      arry.push(i);
+    }
+  }
+  return arry;
+}
+
+function covert(number) {
+  let char = "";
+  let n = parseInt(number / 26);
+  let m = number % 26;
+  if (n === 0) {
+    char = charArry.charAt(m);
+  } else {
+    char = charArry.charAt(n - 1) + charArry.charAt(m);
+  }
+  return char;
 }
 
 module.exports = get_letter_interval;
